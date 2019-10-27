Fix sign convention for fine delays
Hopefully - it could still use a test with comparison to coarse delays.

src/katgpucbf/fgpu/postproc.py
@@ -61,7 +61,7 @@ class Postproc(accel.Operation):
                 np.int32(out.padded_shape[2]),
                 np.int32(in0.padded_shape[1]),
                 np.int32(self.acc_len),
-                np.float32(1 / self.channels),
+                np.float32(-1 / self.channels),
                 np.float32(self.quant_scale)
             ],
             global_size=(self.template.block * groups_x,

test/fgpu/test_postproc.py
@@ -7,9 +7,9 @@ from katfgpu import postproc
 def postproc_host_pol(data, channels, acc_len, spectra, fine_delay, quant_scale):
     # Throw out last channel (Nyquist frequency)
     data = data[:, :channels]
-    # Compute delay phases (TODO: check sign convention and factors of 2)
+    # Compute delay phases
     channel_idx = np.arange(channels, dtype=np.float32)[np.newaxis, :]
-    phase = np.exp(np.complex64(2j * np.pi) * fine_delay[:, np.newaxis] * channel_idx / (2 * channels))
+    phase = np.exp(np.complex64(-2j * np.pi) * fine_delay[:, np.newaxis] * channel_idx / (2 * channels))
     assert phase.dtype == np.complex64
     corrected = data * phase.astype(np.complex64)
     # Split complex into real, imaginary