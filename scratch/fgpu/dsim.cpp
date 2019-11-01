be3187ad8e64ce0d4890210bf868f05152d5d6fc
Bruce Merry <bmerry@ska.ac.za>
2019-11-01 13:47:44 +0200

    Early work towards making a digitiser simulation
    It compiles, but that's about all that can be said for it so far.

---
+++ scratch/fgpu/dsim.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+#include <memory>
+#include <boost/program_options.hpp>
+#include <spead2/send_udp_ibv.h>
+#include <spead2/common_semaphore.h>
+
+namespace po = boost::program_options;
+
+static constexpr double bandwidth = 1712000000.0;
+static constexpr int max_heaps = 1024;
+static constexpr int sample_bits = 10;
+static constexpr int packet_samples = 4096;
+static constexpr std::size_t packet_size = packet_samples * sample_bits / 8;
+static const spead2::flavour flavour(4, 64, 48);
+
+struct heap_data
+{
+    std::unique_ptr<std::uint8_t[]> data;
+    spead2::send::heap heap;
+    spead2::send::heap::item_handle timestamp_handle;
+
+    heap_data()
+        : data(std::make_unique<std::uint8_t[]>(packet_size)),
+        heap(flavour),
+        timestamp_handle(heap.add_item(0x1600, 0))
+    {
+        heap.add_item(0x3101, 0);
+        heap.add_item(0x3102, 0);
+        heap.add_item(0x3300, data.get(), packet_size, false);
+        heap.set_repeat_pointers(true);
+    }
+};
+
+struct polarisation
+{
+    spead2::thread_pool pool;
+    spead2::send::udp_ibv_stream stream;
+    std::vector<heap_data> heaps;
+    spead2::semaphore next_sem;
+    int next = 0;
+    std::int64_t timestamp = 0;
+
+    polarisation(std::size_t capacity,
+                 const boost::asio::ip::udp::endpoint &endpoint,
+                 const boost::asio::ip::address &interface_address)
+        : pool(1),
+        stream(pool, endpoint,
+               spead2::send::stream_config(5120 + 64, bandwidth * 10.0 / 8.0, 65536, capacity),
+               interface_address),
+        next_sem(capacity)
+    {
+        heaps.reserve(capacity);
+        for (std::size_t i = 0; i < capacity; i++)
+            heaps.emplace_back();
+    }
+
+    void send_next()
+    {
+        spead2::semaphore &next_sem = this->next_sem;
+        auto callback = [&next_sem] (const boost::system::error_code &ec, std::size_t)
+        {
+            if (ec)
+                std::cerr << "Error: " << ec;
+            else
+                next_sem.put();
+        };
+        heaps[next].heap.get_item(heaps[next].timestamp_handle).data.immediate = timestamp;
+        timestamp += packet_samples;
+        stream.async_send_heap(heaps[next].heap, callback);
+        next = (next + 1) % heaps.size();
+    }
+};
+
+int main(int argc, char **argv)
+{
+    boost::asio::ip::udp::endpoint endpoint0(
+        boost::asio::ip::address::from_string("239.101.200.0"), 7148);
+    boost::asio::ip::udp::endpoint endpoint1(
+        boost::asio::ip::address::from_string("239.101.200.2"), 7148);
+    auto interface_address = boost::asio::ip::address::from_string("192.168.8.5");
+
+    std::array<std::unique_ptr<polarisation>, 2> pols;
+    pols[0] = std::make_unique<polarisation>(max_heaps, endpoint0, interface_address);
+    pols[1] = std::make_unique<polarisation>(max_heaps, endpoint1, interface_address);
+    while (true)
+    {
+        for (int p = 0; p < 2; p++)
+            semaphore_get(pols[p]->next_sem);
+        for (int p = 0; p < 2; p++)
+            pols[p]->send_next();
+    }
+    return 0;
+}
