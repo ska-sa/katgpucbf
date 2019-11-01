01fb4dc5488fd5fd34b0e0a384a35b2b0df2d036
Bruce Merry <bmerry@ska.ac.za>
2019-11-01 15:00:18 +0200

    Add command-line parser to dsim

--- scratch/fgpu/dsim.cpp
+++ scratch/fgpu/dsim.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <cstdlib>
 #include <memory>
 #include <boost/program_options.hpp>
 #include <spead2/send_udp_ibv.h>
@@ -8,15 +9,62 @@
 
 namespace po = boost::program_options;
 
-static constexpr double bandwidth = 1712000000.0;
+struct options
+{
+    std::string interface;
+    std::vector<std::string> addresses;
+    std::uint16_t port = 7148;
+    int max_heaps = 128;
+    double adc_rate = 1712000000.0;
+};
+
 static constexpr int capacity = 65536;
 static constexpr int sample_bits = 10;
 static constexpr int packet_samples = 4096;
 static constexpr std::size_t packet_size = packet_samples * sample_bits / 8;
 static const spead2::flavour flavour(4, 64, 48);
-static const spead2::send::stream_config config(
-    8872,      // Doesn't matter, just needs to be bigger than actual size
-    bandwidth * 10.0 / 8.0 * (packet_size + 64) / packet_size, 65536, 128);
+
+template<typename T>
+static po::typed_value<T> *make_opt(T &var)
+{
+    return po::value<T>(&var)->default_value(var);
+}
+
+static options parse_options(int argc, const char **argv)
+{
+    options opts;
+    po::options_description desc, hidden, all;
+    desc.add_options()
+        ("interface", po::value(&opts.interface)->required(), "Interface address")
+        ("port", make_opt(opts.port), "Destination UDP port")
+        ("max-heaps", make_opt(opts.max_heaps), "Depth of send queue (per polarisation)")
+        ("adc-rate", make_opt(opts.adc_rate), "Sampling rate")
+    ;
+
+    hidden.add_options()
+        ("address", po::value<std::vector<std::string>>(&opts.addresses)->composing(),
+         "destination IP addresses (one per polarisation)")
+    ;
+    all.add(desc);
+    all.add(hidden);
+    po::positional_options_description positional;
+    positional.add("address", -1);
+    try
+    {
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv)
+            .options(all)
+            .positional(positional)
+            .run(), vm);
+        po::notify(vm);
+    }
+    catch (po::error &e)
+    {
+        std::cerr << e.what() << '\n';
+        std::exit(2);
+    }
+    return opts;
+}
 
 struct heap_data
 {
@@ -45,12 +93,18 @@ struct polarisation
     int next = 0;
     std::int64_t timestamp = 0;
 
-    polarisation(std::size_t capacity,
+    polarisation(const options &opts,
+                 std::size_t capacity,
                  const boost::asio::ip::udp::endpoint &endpoint,
                  const boost::asio::ip::address &interface_address)
         : pool(1),
-        stream(pool, endpoint, config, interface_address),
-        next_sem(config.get_max_heaps())
+        stream(pool, endpoint,
+               spead2::send::stream_config(
+                   packet_size + 128,  // Doesn't matter, just needs to be bigger than actual size
+                   opts.adc_rate * 10.0 / 8.0 * (packet_size + 72) / packet_size,
+                   65536, opts.max_heaps),
+               interface_address),
+        next_sem(opts.max_heaps)
     {
         heaps.reserve(capacity);
         for (std::size_t i = 0; i < capacity; i++)
@@ -74,23 +128,24 @@ struct polarisation
     }
 };
 
-int main(int argc, char **argv)
+int main(int argc, const char **argv)
 {
-    boost::asio::ip::udp::endpoint endpoint0(
-        boost::asio::ip::address::from_string("239.101.200.0"), 7148);
-    boost::asio::ip::udp::endpoint endpoint1(
-        boost::asio::ip::address::from_string("239.101.200.2"), 7148);
-    auto interface_address = boost::asio::ip::address::from_string("192.168.8.2");
+    options opts = parse_options(argc, argv);
 
-    std::array<std::unique_ptr<polarisation>, 2> pols;
-    pols[0] = std::make_unique<polarisation>(capacity, endpoint0, interface_address);
-    pols[1] = std::make_unique<polarisation>(capacity, endpoint1, interface_address);
+    auto interface_address = boost::asio::ip::address::from_string(opts.interface);
+    std::vector<std::unique_ptr<polarisation>> pols;
+    for (const std::string &address : opts.addresses)
+    {
+        boost::asio::ip::udp::endpoint endpoint(
+            boost::asio::ip::address::from_string(address), opts.port);
+        pols.push_back(std::make_unique<polarisation>(opts, capacity, endpoint, interface_address));
+    }
     while (true)
     {
-        for (int p = 0; p < 2; p++)
-            semaphore_get(pols[p]->next_sem);
-        for (int p = 0; p < 2; p++)
-            pols[p]->send_next();
+        for (auto &p : pols)
+            semaphore_get(p->next_sem);
+        for (auto &p : pols)
+            p->send_next();
     }
     return 0;
 }
