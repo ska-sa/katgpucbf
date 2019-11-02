07e2d7fa39997de49517d8d0fa87cce10407a6e7
Bruce Merry <bmerry@ska.ac.za>
2019-11-02 12:35:02 +0000

    Generalise gpu_copy test

--- scratch/fgpu/gpu_copy.py
+++ scratch/fgpu/gpu_copy.py
@@ -2,24 +2,49 @@
 
 import sys
 import time
+import argparse
+import mmap
 
 import numpy as np
 import pycuda.autoinit
 import pycuda.driver
 from pycuda.gpuarray import GPUArray
 
-size = 1024 * 1024 * 1024
-host = pycuda.driver.pagelocked_empty((size,), np.uint8)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--size', '-s', type=int, default=1024*1024*1024)
+parser.add_argument('--repeat', '-r', type=int, default=20)
+parser.add_argument('--forever', action='store_true')
+parser.add_argument('--mem', default='pagelocked', choices=('pagelocked', 'wc', 'huge'))
+parser.add_argument('direction', choices=('htod', 'dtoh'))
+args = parser.parse_args()
+
+size = args.size
+rep = args.repeat
+if args.mem == 'pagelocked':
+    host = pycuda.driver.pagelocked_empty((size,), np.uint8)
+elif args.mem == 'wc':
+    host = pycuda.driver.pagelocked_empty((size,), np.uint8,
+                                          mem_flags=pycuda.driver.host_alloc_flags.WRITECOMBINED)
+elif args.mem== 'huge':
+    mapping = mmap.mmap(-1, size, flags=mmap.MAP_SHARED | 0x40000)  # MAP_HUGETLB
+    array = np.frombuffer(mapping, np.uint8)
+    host = pycuda.driver.register_host_memory(array)
+    assert len(host) == size
+else:
+    parser.error(f'Unknown memory type {args.mem}')
 host.fill(1)
 device = GPUArray((size,), np.uint8)
-rep = 10
+device.set(host)
 while True:
     start = time.time()
     for i in range(rep):
-        if sys.argv[1] == 'htod':
+        if args.direction == 'htod':
             device.set(host)
         else:
             device.get(host)
     stop = time.time()
     rate = size * rep / (stop - start)
-    print(rate / 1e9)
+    print(size * rep, "bytes ", rate / 1e9, "GB/s")
+    if not args.forever:
+        break
