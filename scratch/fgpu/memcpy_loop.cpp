74e37313be7499c176fe28bd273048fcb8210498
Bruce Merry <bmerry@ska.ac.za>
2019-11-04 07:18:17 +0000

    Make memcpy_loop able to do huge pages

--- scratch/fgpu/memcpy_loop.cpp
+++ scratch/fgpu/memcpy_loop.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 #include <cstddef>
 #include <cstring>
 #include <chrono>
+
+#include <sys/mman.h>
 
 using namespace std;
 using namespace std::chrono;
 
 static constexpr std::size_t buffer_size = 128 * 1024 * 1024;
-static constexpr int reps = 20;
+static constexpr int reps = 10;
 static constexpr int passes = 5;
+
+static char *allocate(std::size_t size)
+{
+    int flags = MAP_PRIVATE | MAP_ANONYMOUS;
+    if (0)
+    {
+        flags |= MAP_HUGETLB;
+        std::cout << "Using huge pages\n";
+    }
+    else
+    {
+        std::cout << "Using normal pages\n";
+    }
+    void *addr = mmap(NULL, size, PROT_READ | PROT_WRITE, flags, -1, 0);
+    assert(addr != MAP_FAILED);
+    return (char *) addr;
+}
 
 int main()
 {
     vector<char *> src, dst;
     for (int i = 0; i < reps; i++)
     {
-        src.push_back(new char[buffer_size]);
-        dst.push_back(new char[buffer_size]);
+        src.push_back(allocate(buffer_size));
+        dst.push_back(allocate(buffer_size));
         memset(src.back(), 1, buffer_size);
         memset(dst.back(), 1, buffer_size);
     }
