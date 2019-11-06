429f5fe17a304bd33a20d8c2c28c1729ebaea41e
Bruce Merry <bmerry@ska.ac.za>
2019-11-06 17:15:51 +0200

    Add tests for LinearDelayModel

---
+++ test/fgpu/test_delay.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+import pytest
+
+from katfgpu.delay import LinearDelayModel, MultiDelayModel
+
+
+@pytest.fixture
+def linear() -> LinearDelayModel:
+    return LinearDelayModel(12345, 100.0, 0.25)
+
+
+def test_linear_call(linear: LinearDelayModel) -> None:
+    assert linear(12000.0) == 13.75
+    assert linear(12345.0) == 100.0
+    assert linear(12345.5) == 100.125
+    assert linear(12945.5) == 250.125
+    assert linear(12945.75) == 250.1875
+
+
+def test_linear_invert(linear: LinearDelayModel) -> None:
+    assert linear.invert(12445) == (12345, 0.0)
+    assert linear.invert(13001) == pytest.approx((12790, 0.2))
+    assert linear.invert(12999) == pytest.approx((12788, -0.2))
+
+
+def test_linear_invert_range(linear: LinearDelayModel) -> None:
+    time, residual = linear.invert_range(12999, 13005, 1)
+    np.testing.assert_array_equal(time, [12788, 12789, 12790, 12791, 12791, 12792])
+    np.testing.assert_array_almost_equal(residual, [-0.2, 0.0, 0.2, 0.4, -0.4, -0.2])
+
+    time, residual = linear.invert_range(13000, 14000, 100)
+    exact_time = time - residual
+    forward = exact_time + np.apply_along_axis(linear, 0, exact_time)
+    np.testing.assert_array_almost_equal(forward, np.arange(13000, 14000, 100))
