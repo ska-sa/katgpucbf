c8aff0095d9d569f0f44be9c0119d3115f0195d6
Gareth Callanan <gcallanan@ska.ac.za>
2020-10-13 08:06:54 +0000

    Added comments to the pytest class

--- src/katgpucbf/xbgpu/tensorcore_correlator_test.py
+++ src/katgpucbf/xbgpu/tensorcore_correlator_test.py
@@ -1,16 +1,30 @@
-"""TODO: Write docstring."""
+"""
+Module for performing unit tests on the Tensor core correlation kernel.
 
+Contains two unit tests:
+    1. @test_correlator_exhaustive - this test takes very long to run, but generates a high degree of certainty
+    over the results.
+    2. @test_correlator_quick - this test runs very quickly but is less exhaustive. It is used for quickly testing
+    much larger array sizes.
+
+TODO:
+    1. Once the functionality has been added to the TensorCoreCorrelator class, add a unit test to verify that the
+    kernel can be called multiple times without zeroing the visibility matrix.
+"""
 import pytest
 import numpy as np
 import tensorcore_correlator
 from katsdpsigproc import accel
 
-# Different array sizes that could potentially be used by MeerKAT
+# Array specifying different array sizes that could potentially be used by MeerKAT
 array_size = [4, 8, 16, 32, 64, 84, 192, 256]
 
 
 def get_simple_test_ant_value(channel_index, ant_index):
-    """TODO: Write docstring."""
+    """Hashing function to generate a unique int8 number for a specific antenna channel combination.
+
+    The value returned is between -127 and 127(inclusive) of type np.int8.
+    """
     ant_value = np.uint8(channel_index * ant_index) - 128
     if ant_value == -128:
         ant_value += 1
@@ -25,9 +39,9 @@ def generate_antpair_visibilities_host(bufSamples_host, channel_index, ant1, ant
     the shape (channels, samples_per_channel // times_per_block, dual_pol_ants, polarizastions, times_per_block)
     required by the Tensor core correlation kernels.
 
-    Returns visibilities hh, hv, vh and vv representing different interpolarisation products.
+    This is a naive implementation of the correlation algorithm and is computationally very slow.
 
-    This is a naive implementation of the algorithm and is very slow
+    Returns visibilities hh, hv, vh and vv representing different interpolarisation products.
     """
     hh, hv, vh, vv = 0 + 0j, 0 + 0j, 0 + 0j, 0 + 0j
     for time_index1 in range(0, time_outer_range):
@@ -47,11 +61,18 @@ def generate_antpair_visibilities_host(bufSamples_host, channel_index, ant1, ant
 
 @pytest.mark.parametrize("num_ants", array_size)
 def test_correlator_exhaustive(num_ants):
-    """TODO: Write docstring."""
+    """
+    Intensive unit test of the Tensor core correlation algorithm.
+
+    This unit test runs on random input data. The CPU side check for correctness is very slow - the input sample
+    array size has been kept small to speed this up. For MeerKAT sizes, the input sample array will be much bigger.
+    """
+    # 1. Array parameters
     dual_pol_ants = num_ants
     channels = 2
     samples_per_channel = 16
 
+    # 2. Initialise GPU kernels and buffers.
     ctx = accel.create_some_context(device_filter=lambda x: x.is_cuda)
     queue = ctx.create_command_queue()
 
@@ -67,13 +88,30 @@ def test_correlator_exhaustive(num_ants):
     bufVisibilities_device = tensorCoreCorrelator.buffer("outVisibilities")
     bufVisibilities_host = bufVisibilities_device.empty_like()
 
+    # 3. Generate random input data - need to modify the dtype and  shape of the array as numpy does not have a packet
+    # 8-bit int complex type.
+
+    bufSamplesInt16Shape = template.inputDataShape
+    bufSamplesInt8Shape = list(bufSamplesInt16Shape)
+    bufSamplesInt8Shape[-1] *= 2  # By converting from int16 to int8, the length of the last dimension doubles.
+    bufSamplesInt8Shape = tuple(bufSamplesInt8Shape)
+
+    bufSamples_host.dtype = np.int8
+    bufSamples_host[:] = np.random.randint(
+        -127,
+        high=128,
+        size=bufSamplesInt8Shape,
+        dtype=np.int8,
+    )  # np.ones(bufSamples_host.shape, dtype=np.int8)
+    bufSamples_host.dtype = np.int16
+
+    # 4. Transfer input sample array to the GPU, run kernel, transfer output visibilities array to the CPU.
     bufSamples_device.set(queue, bufSamples_host)
     tensorCoreCorrelator()
     bufVisibilities_device.get(queue, bufVisibilities_host)
 
-    # 8. Check that the GPU visibilities are correct.
-
-    # 8.1 Need to convert the input and output arrays to 32-bit float complex type as numpy has no 8-bit or 32-bit int
+    # 5. Check that the GPU visibilities are correct.
+    # 5.1 Need to convert the input and output arrays to 32-bit float complex type as numpy has no 8-bit or 32-bit int
     # complex type.
     bufSamples_host.dtype = np.int8
     bufSamples_host = bufSamples_host.astype(np.float32)
@@ -84,14 +122,14 @@ def test_correlator_exhaustive(num_ants):
     bufVisibilities_host.dtype = np.csingle
 
     bufCorrectVisibilities_host = np.empty_like(bufVisibilities_host)
-    # 8.2 Generate the visibilities on the CPU - this is not a simple matrix operation due to the indexing of the input
+
+    # 5.2 Generate the visibilities on the CPU - this is not a simple matrix operation due to the indexing of the input
     # samples, I am just going to do a naive brute force for now to be optomised later if needs be.
 
     time_outer_range = samples_per_channel // template._times_per_block
     time_inner_range = template._times_per_block
 
     for channel_index in range(0, channels):
-        print(channel_index)
         for ant1_index in range(0, dual_pol_ants):
             for ant2_index in range(0, ant1_index + 1):
                 hh, hv, vh, vv = generate_antpair_visibilities_host(
@@ -103,30 +141,27 @@ def test_correlator_exhaustive(num_ants):
                 bufCorrectVisibilities_host[channel_index][baseline_index][0][1] = vh
                 bufCorrectVisibilities_host[channel_index][baseline_index][1][1] = vv
 
-                # if(bufVisibilities_host[channel_index][baseline_index][0][0] != hh or
-                #    bufVisibilities_host[channel_index][baseline_index][1][0] != hv or
-                #    bufVisibilities_host[channel_index][baseline_index][0][1] != vh or
-                #    bufVisibilities_host[channel_index][baseline_index][1][1] != vv):
-                #    print("What",channel_index,ant1_index,ant2_index)
-                #    print(hh, bufVisibilities_host[channel_index][baseline_index][0][0])
-                #    print(hv, bufVisibilities_host[channel_index][baseline_index][1][0])
-                #    print(vh, bufVisibilities_host[channel_index][baseline_index][0][1])
-                #    print(vv, bufVisibilities_host[channel_index][baseline_index][1][1])
-                #    temp += 1
-
-    # print(temp,baselines,channels)
-
-    # 8.3 Check that the CPU version and GPU version are identical
+    # 5.3 Check that the CPU version and GPU version are identical
     np.testing.assert_array_equal(bufCorrectVisibilities_host, bufVisibilities_host)
 
 
 @pytest.mark.parametrize("num_ants", array_size)
 def test_correlator_quick(num_ants):
-    """TODO: Write docstring."""
-    dual_pol_ants = num_ants
-    channels = 16
-    samples_per_channel = 256
+    """
+    Lightweight unit test of the Tensor core correlation algorithm.
 
+    This unit test uses a hashing function(@get_simple_test_ant_value(...)) to determine the values in the input
+    sample array for a specific antenna-channel combination. This value is kept constant over all time values. This
+    allows the output visibility values to be calculated quickly on the CPU without performing the full correlation
+    operation. This unit test runs much quicker than @test_correlator_exhaustive, while being much less exhaustive.
+    It is used to perform a quick check for the correctness on much larger input sample array sizes.
+    """
+    # 1. Array parameters
+    dual_pol_ants = num_ants
+    channels = 64
+    samples_per_channel = 3072
+
+    # 2. Initialise GPU kernels and buffers.
     ctx = accel.create_some_context(device_filter=lambda x: x.is_cuda)
     queue = ctx.create_command_queue()
 
@@ -137,7 +172,6 @@ def test_correlator_quick(num_ants):
     correlator.ensure_all_bound()
 
     time_outer_range = samples_per_channel // template._times_per_block
-    # time_inner_range = template._times_per_block
 
     bufSamples_device = correlator.buffer("inSamples")
     bufSamples_host = bufSamples_device.empty_like()
@@ -145,7 +179,7 @@ def test_correlator_quick(num_ants):
     bufVisibilities_device = correlator.buffer("outVisibilities")
     bufVisibilities_host = bufVisibilities_device.empty_like()
 
-    # Generate predictable values
+    # 3. Generate predictable input data. The time samples remain constant for every antenna-channel combination.
     bufSamples_host.dtype = np.int8
     for channel_index in range(channels):
         for ant_index in range(dual_pol_ants):
@@ -154,25 +188,23 @@ def test_correlator_quick(num_ants):
                 bufSamples_host[channel_index][time_outer_index][ant_index] = np.full(
                     bufSamples_host.shape[3:], sample_value, dtype=np.int8
                 )
-    print(bufSamples_host.shape[3:])
     bufSamples_host.dtype = np.int16
-    print(bufSamples_host.shape[3:])
 
+    # 4. Transfer input sample array to the GPU, run kernel, transfer output visibilities array to the CPU.
     bufSamples_device.set(queue, bufSamples_host)
     correlator()
     bufVisibilities_device.get(queue, bufVisibilities_host)
 
-    # 8. Check that the GPU visibilities are correct.
-
-    # 8.1 Need to convert the input and output arrays to 32-bit float complex type as numpy has no 8-bit or 32-bit int
-    # complex type.
+    # 5. Check that the GPU visibilities are correct.
+    # 5.1 Need to convert the output array to 32-bit float complex type as numpy has no  32-bit int complex type.
 
     bufVisibilities_host.dtype = np.int32
     bufVisibilities_host = bufVisibilities_host.astype(np.float32)
     bufVisibilities_host.dtype = np.csingle
 
+    # 5.2 Generate the visibilities on the CPU and check they match the expected value. The output values are simple to
+    # calculate and do not require performing the matrix multiple as it is performed on the GPU.
     for channel_index in range(0, channels):
-        print(channel_index)
         for ant1_index in range(0, dual_pol_ants):
             for ant2_index in range(0, ant1_index + 1):
                 ant1_value = get_simple_test_ant_value(channel_index, ant1_index)
