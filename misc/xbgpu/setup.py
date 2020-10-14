6ca3c2beef80af35e3f87f7367a9f248c0db090e
Gareth Callanan <gcallanan@ska.ac.za>
2020-10-14 08:51:30 +0000

    Created setup.py file for installing the katxgpu package

---
+++ misc/xbgpu/setup.py
@@ -0,0 +1,26 @@
+"""Script for installing the katxgpu package."""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="katxgpu",
+    version="0.0.1",
+    author="Gareth Callanan",
+    author_email="gcallanan@ska.ac.za",
+    description="GPU-accelerated X-Engine for the MeerKAT extension correlator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ska-sa/katxgpu",
+    packages=setuptools.find_packages(),
+    package_data={"": ["kernels/*.mako"]},
+    include_package_data=True,
+    classifiers=[
+        # "License :: OSI Approved :: GNU General Public License v2 (GPLv2)", #I am not sure what is required here
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+    python_requires=">=3.6",
+)
