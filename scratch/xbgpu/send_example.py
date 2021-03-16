32efdc6a8cfa98496d32bc6a3e664f7d3fd8c557
Gareth Callanan <gcallanan@ska.ac.za>
2021-03-16 14:24:51 +0000

    Basic send_example is working.

--- scratch/xbgpu/send_example.py
+++ scratch/xbgpu/send_example.py
@@ -1,5 +1,28 @@
 """TODO: Write this."""
 
 import katxgpu.xsend
+import asyncio
 
 x = katxgpu.xsend.XEngineSPEADSend()
+
+x.send_heap(0x1, x._free_heaps_queue.get())
+x.send_heap(0x2, x._free_heaps_queue.get())
+x.send_heap(0x3, x._free_heaps_queue.get())
+x.send_heap(0x4, x._free_heaps_queue.get())
+x.send_heap(0x5, x._free_heaps_queue.get())
+
+
+async def send_process():
+    """TODO: Write this docstring."""
+    print("Sending.")
+    num_sent = 0
+    while 1:
+        num_sent += 1
+        await x.get_free_heap()
+        print(num_sent)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_process())
+    loop.close()
