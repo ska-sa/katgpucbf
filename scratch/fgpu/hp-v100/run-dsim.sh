30a5de31e05b48676dc81b065cb4513853c5b46b
Bruce Merry <1963944+bmerry@users.noreply.github.com>
2021-07-08 16:41:32 +0200

    Add scratch script for running dsims on hp-v100 (#29)
    Also add a tmuxp config file for launching four dsims.

---
+++ scratch/fgpu/hp-v100/run-dsim.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -u
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 0|1|2|3" 1>&2
+    exit 2
+fi
+
+case "$1" in
+    0|1|2|3)
+        ;;
+    *)
+        echo "Pass an integer from 0-3" 1>&2
+        exit 2
+        ;;
+esac
+
+cpu="$1"
+addresses="239.102.$1.64+7:7148 239.102.$1.72+7:7148"
+ip="10.100.$((53+4*$1)).1"
+exec cap_net_raw numactl -C $cpu ../dsim --interface $ip --adc-rate 1712000000 --ttl 2 $addresses
