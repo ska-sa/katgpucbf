d9e3572aed724c2a0071af7074f70a9a30034e0f
James Smith <james-smith-za@users.noreply.github.com>
2021-07-09 13:31:27 +0300

    Document dev-setup script

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -e # So we can stop on a failure instead of bumbling along.
 
+echo "Creating a virtual env for katcbfgpu development"
 python3.8 -m venv .venv
+echo "Activating the virtual env"
 source .venv/bin/activate
 
+echo "Installing package's requirements"
 pip install -r requirements.txt
+echo "Installing dev requirements"
 pip install -r requirements-dev.txt
+echo "Installing doc requirements"
 pip install -r requirements-doc.txt
 
+echo "Setting up pre-commit"
 pre-commit install
