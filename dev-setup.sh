5079a31ec0cabd47a6fbba4c9e7eb251d81fe23d
James Smith <jsmith@ska.ac.za>
2021-07-09 10:56:15 +0000

    Add wheel to the venv
    It lets pip do its thing just that much smoother. Don't know why it
    isn't included in the environments that the built-in venv creates,
    because virtualenv included it in days of yore.

--- dev-setup.sh
+++ dev-setup.sh
@@ -6,6 +6,7 @@ python3.8 -m venv .venv
 echo "Activating the virtual env"
 source .venv/bin/activate
 
+pip install wheel  # Makes the process a bit slicker.
 echo "Installing package's requirements"
 pip install -r requirements.txt
 echo "Installing dev requirements"
@@ -15,3 +16,5 @@ pip install -r requirements-doc.txt
 
 echo "Setting up pre-commit"
 pre-commit install
+
+echo "Done. Don't forget to source the new venv before you start!"
