2298b24d287dddd83d089f165dc9a93f986abfc8
Bruce Merry <bmerry@ska.ac.za>
2021-07-19 16:47:35 +0200

    Update test parameters
    - Remove really large arrays (not needed for MK+)
    - Add 8192 channels
    
    This makes testing a bit faster.

--- test/xbgpu/test_parameters.py
+++ test/xbgpu/test_parameters.py
@@ -1,9 +1,9 @@
 """Module containing correlator configuration options that should be tested where appropriate in all unit tests."""
 
-# These are the estimated subarray sizes that will be run. The 130, 192 and 256 values are estimates for SKA and are
-# not final. Additionally values 5,23,61 and 19 are just there to test that various non-power-of-two array sizes will
+# These are the estimated subarray sizes that will be run.  Values 5, 23, 61 and
+# 19 are just there to test that various non-power-of-two array sizes will
 # run.
-array_size = [4, 8, 16, 32, 64, 84, 130, 192, 256, 5, 23, 61, 19]
+array_size = [5, 23, 61, 19, 4, 8, 16, 32, 64, 84]
 
 # This is always set to 256 for the MeerKAT case and leads to packet sizes equal to 256 * 4 bytes per sample = 1 KiB.
 # There is a chance that for the MeerKAT extension this could be configured to allow for packet sizes equal to 2 or 4
@@ -11,4 +11,4 @@ array_size = [4, 8, 16, 32, 64, 84, 130, 192, 256, 5, 23, 61, 19]
 num_samples_per_channel = [256, 512, 1024]
 
 # Number of FFT channels out of the F-Engine
-num_channels = [1024, 4096, 32768]
+num_channels = [1024, 4096, 8192, 32768]
