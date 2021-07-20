073f186c33cdf8bafe04055618e3afb3038c2b31
James Smith <james-smith-za@users.noreply.github.com>
2021-07-20 07:21:19 +0000

    Move fgpu test fixtures to conftest.py
    They're going to be used by multiple different test files, so makes
    sense to have them available.
    
    Also remove the module-scoping, to avoid changes in state affecting
    individual unit tests.

---
+++ test/fgpu/conftest.py
@@ -0,0 +1,40 @@
+"""Fixtures for use in fgpu unit tests."""
+
+import aiokatcp
+import katsdpsigproc.accel as accel
+import pytest
+
+from katgpucbf.fgpu.main import make_engine
+
+
+@pytest.fixture
+async def gpu_context():
+    """Generate a GPU context."""
+    ctx = accel.create_some_context(device_filter=lambda x: x.is_cuda, interactive=False)
+    return ctx
+
+
+@pytest.fixture
+async def engine_server(request, gpu_context):
+    """Create a dummy :class:`.fgpu.Engine` for unit testing.
+
+    The arguments passed are based on the default arguments from
+    :mod:`~katgpucbf.fgpu.main`, and are a set of simple parameters just to
+    get the :class:`~.fgpu.Engine` running so that the KATCP interface can be
+    tested.
+    """
+    server = make_engine(gpu_context, arglist=request.cls.engine_arglist)
+
+    await server.start()
+    yield server
+    await server.stop()
+
+
+@pytest.fixture
+async def engine_client(engine_server):
+    """Create a KATCP client for communicating with the dummy server."""
+    address = engine_server.server.sockets[0].getsockname()
+    client = await aiokatcp.Client.connect(address[0], address[1])
+    yield client
+    client.close()
+    await client.wait_closed()
--- test/fgpu/test_katcp_interface.py
+++ test/fgpu/test_katcp_interface.py
@@ -1,42 +1,16 @@
 """Collection of tests for the KATCP interface of katgpucbf.fgpu."""
 
-import asyncio
 import time
 
-import aiokatcp
-import katsdpsigproc.accel as accel
 import pytest
-
-from katgpucbf.fgpu.main import make_engine
 
 pytestmark = pytest.mark.asyncio
 
 
-@pytest.fixture(scope="module")
-def event_loop():
-    """Re-define the ``event_loop`` fixture, to give it a module scope."""
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
+class TestKatcpRequests:
+    """Unit tests for the Engine's KATCP requests."""
 
-
-@pytest.fixture(scope="module")
-async def gpu_context():
-    """Generate a GPU context."""
-    ctx = accel.create_some_context(device_filter=lambda x: x.is_cuda, interactive=False)
-    return ctx
-
-
-@pytest.fixture(scope="module")
-async def engine_server(gpu_context):
-    """Create a dummy :class:`.fgpu.Engine` for unit testing.
-
-    The arguments passed are based on the default arguments from
-    :mod:`~katgpucbf.fgpu.main`, and are a set of simple parameters just to
-    get the :class:`~.fgpu.Engine` running so that the KATCP interface can be
-    tested.
-    """
-    arglist = [
+    engine_arglist = [
         "--katcp-port=0",
         "--src-interface=lo",
         "--dst-interface=lo",
@@ -45,25 +19,6 @@ async def engine_server(gpu_context):
         "239.10.10.8+7:7149",  # src2
         "239.10.11.0+15:7149",  # dst
     ]
-    server = make_engine(gpu_context, arglist=arglist)
-
-    await server.start()
-    yield server
-    await server.stop()
-
-
-@pytest.fixture
-async def engine_client(engine_server):
-    """Create a KATCP client for communicating with the dummy server."""
-    address = engine_server.server.sockets[0].getsockname()
-    client = await aiokatcp.Client.connect(address[0], address[1])
-    yield client
-    client.close()
-    await client.wait_closed()
-
-
-class TestKatcpRequests:
-    """Unit tests for the Engine's KATCP requests."""
 
     async def test_quant_gain_set(self, engine_client, engine_server):
         """Test that the quant gain is correctly set."""
