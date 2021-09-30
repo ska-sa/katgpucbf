791597b31cb5b36e9f99e1526a94bfd6fa0de619
Bruce Merry <bmerry@ska.ac.za>
2021-09-30 16:38:53 +0200

    Remove some dangling references to _katgpucbf

--- src/katgpucbf/fgpu/__init__.py
+++ src/katgpucbf/fgpu/__init__.py
@@ -15,7 +15,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-
-from ._katfgpu import Empty, Stopped
-
-__all__ = ["Stopped", "Empty"]
