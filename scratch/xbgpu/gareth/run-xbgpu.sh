afaa209725d39bbce1950b3554cd8c8f77edda5c
Bruce Merry <bmerry@ska.ac.za>
2021-10-29 15:01:37 +0000

    Add script scripts for running xbgpu on gareth
    It runs 8 "double-weight" X engines (256/32768 channels), spread evenly
    over the CCXs and ports of a NIC, all on the 3080 Ti.

---
+++ scratch/xbgpu/gareth/run-xbgpu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e -u
+
+iface1=enp130s0f0
+iface2=enp130s0f1
+channels_per_substream=256
+
+affinity="$(($1 * 3))"
+rx_affinity=$(($affinity+1))
+rx_comp=$affinity
+tx_affinity=$affinity
+other_affinity=$affinity
+src_mcast="239.10.10.$((10 + $1)):7148"
+dst_mcast="239.10.11.$((10 + $1)):7148"
+port="$((7150 + $1))"
+channel_offset=$(($channels_per_substream * $1))
+
+case "$1" in
+    0|1|2|3)
+        iface="$iface1"
+        export CUDA_VISIBLE_DEVICES=0
+        ;;
+    4|5|6|7)
+        iface="$iface2"
+        export CUDA_VISIBLE_DEVICES=0
+        ;;
+    *)
+        echo "Pass 0-7" 1>&2
+        exit 2
+        ;;
+esac
+
+set -x
+
+exec spead2_net_raw numactl -C $other_affinity xbgpu \
+    --src-affinity $rx_affinity \
+    --src-comp-vector $rx_comp \
+    --dst-affinity $tx_affinity \
+    --dst-interface $iface \
+    --src-interface $iface \
+    --adc-sample-rate 1712000000 \
+    --array-size 64 \
+    --spectra-per-heap 256 \
+    --channels 32768 \
+    --channels-per-substream $channels_per_substream \
+    --channel-offset-value $channel_offset \
+    --katcp-port $port \
+    $src_mcast $dst_mcast
