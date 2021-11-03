8dacb45065a7b4978b82fd213cf0cba8a7f12190
Bruce Merry <bmerry@ska.ac.za>
2021-11-03 13:27:57 +0000

    Modify run-fsim scratch script for 2 instances

--- scratch/xbgpu/qgpu01/run-fsim.sh
+++ scratch/xbgpu/qgpu01/run-fsim.sh
@@ -7,14 +7,14 @@ iface_ip2=10.100.41.1
 mcast="239.10.10.$((10 + $1 * 2))+1:7148"
 
 case "$1" in
-    0|1)
+    0)
         iface_ip=$iface_ip1
         ;;
-    2|3)
+    1)
         iface_ip=$iface_ip2
         ;;
     *)
-        echo "Pass 0-3" 1>&2
+        echo "Pass 0-1" 1>&2
         exit 2
         ;;
 esac
