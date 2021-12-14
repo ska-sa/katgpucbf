aedb528ca7f43873d694ee4412e408316354bab6
Bruce Merry <bmerry@ska.ac.za>
2021-12-14 07:39:14 +0000

    Update interface names on gareth in scratch scripts

--- scratch/fgpu/gareth/run-fgpu.sh
+++ scratch/fgpu/gareth/run-fgpu.sh
@@ -3,11 +3,11 @@ set -e -u
 
 # This NIC seems to be dropping a lot of incoming packets
 # (maybe contending with GPU on the same host bridge?)
-# iface1=enp129s0f0np0
-# iface2=enp129s0f1np1
+# iface1=enp129s0f0
+# iface2=enp129s0f1
 
-iface1=enp130s0f0
-iface2=enp130s0f1
+iface1=enp193s0f0
+iface2=enp193s0f1
 cuda1=0
 cuda2=0
 
