f4f2708932aee490dcee50f17d85a14f92283df2
James Smith <jsmith@ska.ac.za>
2022-04-22 12:25:49 +0000

    Add a recv_stream fixture for qualification
    And some genral-purpose cleanup.

--- qualification/__init__.py
+++ qualification/__init__.py
@@ -1,8 +1,36 @@
 """A few handy things intended for correlator qualification."""
-from typing import Union
+import logging
+from dataclasses import dataclass
+from typing import List, Tuple, Union
 
 import aiokatcp
+import numba
+import numpy as np
+import scipy
+import spead2
+import spead2.recv
+import spead2.recv.asyncio
 from katsdptelstate.endpoint import Endpoint, endpoint_parser
+from numba import types
+from spead2.numba import intp_to_voidptr
+from spead2.recv.numba import chunk_place_data
+
+logger = logging.getLogger(__name__)
+CPLX = 2
+
+
+@dataclass
+class Band:
+    """Holds presets for a known band."""
+
+    adc_sample_rate: float
+    centre_frequency: float
+
+
+BANDS = {
+    "l": Band(adc_sample_rate=1712e6, centre_frequency=1284e6),
+    "u": Band(adc_sample_rate=1088e6, centre_frequency=816e6),
+}
 
 
 async def get_sensor_val(client: aiokatcp.Client, sensor_name: str) -> Union[int, float, str]:
@@ -23,13 +51,100 @@ async def get_sensor_val(client: aiokatcp.Client, sensor_name: str) -> Union[int
 
 
 async def get_dsim_endpoint(pc_client: Endpoint, adc_sample_rate: float) -> Endpoint:
-    """Get the katcp address for a named product controller from the master."""
+    """Get the katcp address for a dsim on a product controller.
+
+    The assumption is made that a single dsim is used, with the name
+    ``sim.m800`` suffixed with the ADC sample rate.
+    """
     return endpoint_parser(None)(await get_sensor_val(pc_client, f"sim.m800.{int(adc_sample_rate)}.0.port"))
 
 
 class CorrelatorRemoteControl:
-    """A remote control for the correlator created by the fixture."""
+    """A container class for katcp clients needed by qualification tests."""
 
     def __init__(self, product_controller_client: aiokatcp.Client, dsim_client: aiokatcp.Client) -> None:
         self.product_controller_client = product_controller_client
         self.dsim_client = dsim_client
+
+
+def create_stream(
+    interface_address: str,
+    multicast_endpoints: List[Tuple[str, int]],
+    n_bls: int,
+    n_chans: int,
+    n_chans_per_substream: int,
+    n_bits_per_sample: int,
+    n_spectra_per_acc: int,
+    int_time: float,
+    n_samples_between_spectra: int,
+    use_ibv: bool = False,
+) -> spead2.recv.ChunkRingStream:
+    """Create a spead2 recv stream."""
+    # Lifted from :class:`katgpucbf.xbgpu.XSend`.
+    HEAP_PAYLOAD_SIZE = n_chans_per_substream * n_bls * CPLX * n_bits_per_sample // 8  # noqa: N806
+    HEAPS_PER_CHUNK = n_chans // n_chans_per_substream  # noqa: N806
+
+    # According to the ICD.
+    TIMESTAMP = 0x1600  # noqa: N806
+    FREQUENCY = 0x4103  # noqa: N806
+
+    # Needed for placing the individual heaps within the chunk.
+    items = [FREQUENCY, TIMESTAMP, spead2.HEAP_LENGTH_ID]
+    timestamp_step = n_samples_between_spectra * n_spectra_per_acc
+
+    # Heap placement function. Gets compiled so that spead2's C code can call it.
+    # A chunk consists of all channels and all baselines for a single point in time.
+    @numba.cfunc(types.void(types.CPointer(chunk_place_data), types.uintp), nopython=True)
+    def chunk_place(data_ptr, data_size):
+        data = numba.carray(data_ptr, 1)
+        items = numba.carray(intp_to_voidptr(data[0].items), 3, dtype=np.int64)
+        channel_offset = items[0]
+        timestamp = items[1]
+        payload_size = items[2]
+        # If the payload size doesn't match, discard the heap (could be descriptors etc).
+        if payload_size == HEAP_PAYLOAD_SIZE:
+            data[0].chunk_id = timestamp // timestamp_step
+            data[0].heap_index = channel_offset // n_chans_per_substream
+            data[0].heap_offset = data[0].heap_index * HEAP_PAYLOAD_SIZE
+
+    stream_config = spead2.recv.StreamConfig(substreams=HEAPS_PER_CHUNK)
+
+    # Assuming X-engines are at most 1 second out of sync with each other, with
+    # one extra chunk for luck. May need to revisit that assumption for much
+    # larger array sizes.
+    max_chunks = round(1 // int_time) + 1
+
+    chunk_stream_config = spead2.recv.ChunkStreamConfig(
+        items=items,
+        max_chunks=max_chunks,
+        place=scipy.LowLevelCallable(chunk_place.ctypes, signature="void (void *, size_t)"),
+    )
+
+    free_ringbuffer = spead2.recv.ChunkRingbuffer(max_chunks)
+    data_ringbuffer = spead2.recv.asyncio.ChunkRingbuffer(max_chunks)
+
+    stream = spead2.recv.ChunkRingStream(
+        spead2.ThreadPool(),
+        stream_config,
+        chunk_stream_config,
+        data_ringbuffer,
+        free_ringbuffer,
+    )
+
+    for _ in range(max_chunks):
+        chunk = spead2.recv.Chunk(
+            present=np.empty(HEAPS_PER_CHUNK, np.uint8),
+            data=np.empty((n_chans, n_bls, CPLX), dtype=getattr(np, f"int{n_bits_per_sample}")),
+        )
+        stream.add_free_chunk(chunk)
+
+    if use_ibv:
+        config = spead2.recv.UdpIbvConfig(
+            endpoints=multicast_endpoints, interface_address=interface_address, buffer_size=int(16e6), comp_vector=-1
+        )
+        stream.add_udp_ibv_reader(config)
+    else:
+        for ep in multicast_endpoints:
+            stream.add_udp_reader(*ep, interface_address=interface_address)
+
+    return stream
--- qualification/conftest.py
+++ qualification/conftest.py
@@ -1,28 +1,35 @@
 """Fixtures and options for qualification testing of the correlator."""
 import json
 import logging
-from dataclasses import dataclass
 
 import aiokatcp
 import pytest
+import spead2.recv
+from katsdpservices import get_interface_address
+from katsdptelstate.endpoint import endpoint_list_parser
 
-from . import CorrelatorRemoteControl, get_dsim_endpoint
+from . import BANDS, Band, CorrelatorRemoteControl, create_stream, get_dsim_endpoint, get_sensor_val
+
+logger = logging.getLogger(__name__)
 
 
 def pytest_addoption(parser, pluginmanager):  # noqa: D103
-    # I'm adding the image override as a cmd-line parameter. This process
-    # basically doesn't work without it, because you can't be sure what the MC
-    # is going to use. It seems best that it be explicit.
+    # I'm adding the image override as a cmd-line parameter. It seems best (at
+    # this stage anyway) that it be explicit what image you're testing.
     parser.addoption(
         "--image-override", action="append", required=True, metavar="NAME:IMAGE:TAG", help="Override a single image"
     )
-    # On the other hand, these things aren't likely to change very frequently,
-    # so an INI file is probably appropriate.
+    # On the other hand, the MC details aren't likely to change frequently, so
+    # an INI file is probably appropriate.
     parser.addini("master_controller_host", "Hostname (or IP address) of the SDP master controller", type="string")
     parser.addini("master_controller_port", "TCP port of the SDP master controller", type="string", default="5001")
+    # I'm on the fence about these. Probably on a given machine, you'd set and
+    # forget.
+    parser.addini("interface", "Name of network to use for ingest.", type="string")
+    parser.addini("use_ibv", "Use ibverbs", type="bool", default="false")
 
 
-@pytest.fixture(params=[4, 5])
+@pytest.fixture(params=[2, 3])
 def n_antennas(request):  # noqa: D401
     """Number of antennas, i.e. size of the array."""
     return request.param
@@ -32,20 +39,6 @@ def n_antennas(request):  # noqa: D401
 def n_channels(request):  # noqa: D401
     """Number of channels for the channeliser."""
     return request.param
-
-
-@dataclass
-class Band:
-    """Holds presets for a known band."""
-
-    adc_sample_rate: float
-    centre_frequency: float
-
-
-BANDS = {
-    "l": Band(adc_sample_rate=1712e6, centre_frequency=1284e6),
-    "u": Band(adc_sample_rate=1088e6, centre_frequency=816e6),
-}
 
 
 @pytest.fixture(params=["l", "u"])
@@ -104,6 +97,7 @@ async def correlator_config(pytestconfig, n_antennas: int, n_channels: int, band
         "int_time": int_time,
     }
 
+    logger.debug("Config for correlator generated.")
     return config
 
 
@@ -116,6 +110,7 @@ async def correlator(pytestconfig, correlator_config, band: Band):
     host = pytestconfig.getini("master_controller_host")
     port = int(pytestconfig.getini("master_controller_port"))
     try:
+        logger.debug("Connecting to master controller to try and create a correlator.")
         client = await aiokatcp.Client.connect(host, port)
         async with client:
             reply, _ = await client.request(
@@ -123,7 +118,7 @@ async def correlator(pytestconfig, correlator_config, band: Band):
             )
         product_controller_host = reply[1].decode()
         product_controller_port = int(reply[2].decode())
-        logging.info(
+        logger.info(
             f"Product controller for qualification correlator is at {product_controller_host}:{product_controller_port}"
         )
         product_controller_client = await aiokatcp.Client.connect(product_controller_host, product_controller_port)
@@ -131,12 +126,52 @@ async def correlator(pytestconfig, correlator_config, band: Band):
         dsim_client = await aiokatcp.Client.connect(dsim_host, dsim_port)
         yield CorrelatorRemoteControl(product_controller_client, dsim_client)
     except ConnectionError:
-        logging.exception("unable to connect")
+        logger.exception("unable to connect")
         raise
     except aiokatcp.FailReply:
-        logging.exception("Something went wrong with starting the correlator!")
+        logger.exception("Something went wrong with starting the correlator!")
         raise
     finally:
-        await dsim_client.close()
+        dsim_client.close()
         await product_controller_client.request("product-deconfigure")
         product_controller_client.close()
+
+
+@pytest.fixture
+async def receive_stream(pytestconfig, correlator: CorrelatorRemoteControl) -> spead2.recv.ChunkRingStream:
+    """Create a spead2 receive stream for ingesting X-engine output."""
+    interface_address = get_interface_address(pytestconfig.getini("interface"))
+    # This will require running pytest with spead2_net_raw which is unusual.
+    use_ibv = pytestconfig.getini("use_ibv")
+
+    # I'm still in two minds as to whether to pass these things around as data
+    # members of the CorrelatorRemoteControl class, or to get them this way
+    # via katcp.
+    pc_client = correlator.product_controller_client
+    n_bls = await get_sensor_val(pc_client, "baseline_correlation_products-n-bls")
+    n_chans = await get_sensor_val(pc_client, "baseline_correlation_products-n-chans")
+    n_chans_per_substream = await get_sensor_val(pc_client, "baseline_correlation_products-n-chans-per-substream")
+    n_bits_per_sample = await get_sensor_val(pc_client, "baseline_correlation_products-xeng-out-bits-per-sample")
+    n_spectra_per_acc = await get_sensor_val(pc_client, "baseline_correlation_products-n-accs")
+    int_time = await get_sensor_val(pc_client, "baseline_correlation_products-int-time")
+    n_samples_between_spectra = await get_sensor_val(pc_client, "antenna_channelised_voltage-n-samples-between-spectra")
+
+    multicast_endpoints = [
+        tuple(endpoint)
+        for endpoint in endpoint_list_parser(7148)(
+            await get_sensor_val(pc_client, "baseline_correlation_products-destination")
+        )
+    ]
+
+    return create_stream(
+        interface_address=interface_address,
+        multicast_endpoints=multicast_endpoints,
+        n_bls=n_bls,
+        n_chans=n_chans,
+        n_chans_per_substream=n_chans_per_substream,
+        n_bits_per_sample=n_bits_per_sample,
+        n_spectra_per_acc=n_spectra_per_acc,
+        int_time=int_time,
+        n_samples_between_spectra=n_samples_between_spectra,
+        use_ibv=use_ibv,
+    )
