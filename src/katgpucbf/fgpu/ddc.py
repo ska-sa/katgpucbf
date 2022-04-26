348e0b3d2099a7647b49b731b58cf403b63beedf
Bruce Merry <bmerry@sarao.ac.za>
2022-04-26 13:38:50 +0000

    Tweak a parameter for DDC kernel

--- src/katgpucbf/fgpu/ddc.py
+++ src/katgpucbf/fgpu/ddc.py
@@ -57,7 +57,7 @@ class DDCTemplate:
             raise ValueError("taps must be a multiple of decimation")
         # TODO: tune the magic numbers and enforce more requirements
         self.wgs = 128
-        self._sg_size = 8
+        self._sg_size = 4
         self._coarsen = 4
         self.taps = taps
         self.decimation = decimation
