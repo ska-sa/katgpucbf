fc7c2b0478e45d7b4dbc215f1ee674b7fa721946
Bruce Merry <bmerry@sarao.ac.za>
2022-05-23 14:35:14 +0000

    Fix some issues with compute_bench.py
    The main issue is that the --taps default was incorrect.

--- scratch/fgpu/benchmarks/compute_bench.py
+++ scratch/fgpu/benchmarks/compute_bench.py
@@ -25,7 +25,7 @@ from katgpucbf.fgpu.compute import ComputeTemplate
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--taps", type=int, default=256)
+    parser.add_argument("--taps", type=int, default=16)
     parser.add_argument("--samples", type=int, default=16 * 1024 * 1024)
     parser.add_argument("--channels", type=int, default=32768)
     parser.add_argument("--spectra-per-heap", type=int, default=256)
@@ -37,10 +37,11 @@ def main():
         template = ComputeTemplate(context, args.taps)
         command_queue = context.create_tuning_command_queue()
         extra_samples = (args.taps - 1) * args.channels * 2
+        spectra = args.samples // (args.channels * 2)
         fn = template.instantiate(
             command_queue,
             samples=args.samples + extra_samples,
-            spectra=args.samples // (args.channels * 2),
+            spectra=spectra,
             spectra_per_heap=args.spectra_per_heap,
             channels=args.channels,
         )
@@ -49,10 +50,15 @@ def main():
         fn.ensure_all_bound()
         for name in ["weights", "fine_delay", "phase", "gains"]:
             fn.buffer(name).zero(command_queue)
-        fn()  # Warmup pass
+
+        def run():
+            fn.run_frontend([fn.buffer("in0"), fn.buffer("in1")], [0, 0], 0, spectra)
+            fn.run_backend(fn.buffer("out"))
+
+        run()  # Warmup pass
         start = command_queue.enqueue_marker()
         for _ in range(args.passes):
-            fn()
+            run()
         stop = command_queue.enqueue_marker()
         command_queue.finish()
         average = stop.time_since(start) / args.passes
