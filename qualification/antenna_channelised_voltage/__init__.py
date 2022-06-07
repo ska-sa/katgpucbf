9e3d8f91780b129bed239011c1c22f4b27d97697
Bruce Merry <bmerry@sarao.ac.za>
2022-06-07 10:12:02 +0000

    Add infrastructure for antenna_channelised_voltage tests
    No tests are added yet, but this provides a utility for measuring the
    response to different tones, accelerated by distributing the work across
    all the dsims (and both pols of each).
    
    I've tested it with some code that's still a WIP, so I'm reasonably
    confident that it's correct, or at least it seems to produce a
    reasonable channel shape.

---
+++ qualification/antenna_channelised_voltage/__init__.py
@@ -0,0 +1,107 @@
+################################################################################
+# Copyright (c) 2022, National Research Foundation (SARAO)
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#   https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+"""Tests for the F-engines.
+
+These use the X-engine output, but only as a way to obtain information about
+what is coming out of the F-engines rather than to test the X-engines.
+"""
+
+import asyncio
+from typing import List, Tuple
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+from katgpucbf import N_POLS
+
+from .. import BaselineCorrelationProductsReceiver, CorrelatorRemoteControl
+
+
+async def sample_tone_response(
+    rel_freqs: ArrayLike,
+    amplitude: ArrayLike,
+    correlator: CorrelatorRemoteControl,
+    receiver: BaselineCorrelationProductsReceiver,
+) -> np.ndarray:
+    """Measure power response to tones.
+
+    The input arrays are broadcast with each other. The rest has the same
+    dimensions plus an axis for frequency in the response.
+
+    Parameters
+    ----------
+    rel_freqs
+        Frequencies to measure, in units of channels (integers give the centre
+        of frequency bins).
+    amplitude
+        Amplitude of the tones, on a scale of 0 to 1.
+    correlator
+        Connection to the correlator.
+    receiver
+        Receiver for obtaining the output data.
+    """
+    # Identify auto-correlation baselines corresponding to the dsim outputs.
+    # The fixtures set up a one-to-one relationship between dsims and antennas.
+    # katsdpcontroller ensures that the dsim is started with V pol first and
+    # the fixtures put V pol first in the axis ordering, so this is fairly
+    # straightforward.
+    assert len(correlator.input_labels) == N_POLS * len(correlator.dsim_clients)
+    autos = []
+    for inp in correlator.input_labels:
+        autos.append(correlator.bls_ordering.index((inp, inp)))
+
+    channel_width = correlator.bandwidth / correlator.n_chans
+    freqs = np.asarray(rel_freqs) * channel_width  # Baseband, which is what dsims work on
+    amplitude = np.asarray(amplitude)
+    out_shape = np.broadcast_shapes(freqs.shape, amplitude.shape) + (correlator.n_chans,)
+    out = np.empty(out_shape, np.int32)
+    # Each element is an (out_index, signal_spec) pair. When it fills up to the
+    # number of inputs available, `flush` is called.
+    tasks: List[Tuple[Tuple[int, ...], str]] = []
+
+    async def flush() -> None:
+        """Execute all the work in `tasks`."""
+        # Each dsim has two inputs. Simplify handling an odd number of tasks
+        # (in the last batch) by just duplicating the last task. We'll end up
+        # writing to the output twice, but it's not a big performance issue.
+        while len(tasks) % N_POLS:
+            tasks.append(tasks[-1])
+        requests = []
+        n_dsims = len(tasks) // N_POLS
+        for i in range(n_dsims):
+            signal = ""
+            for j in range(N_POLS):
+                signal += tasks[i * N_POLS + j][1]
+            requests.append(asyncio.create_task(correlator.dsim_clients[i].request("signals", signal)))
+        replies = await asyncio.gather(*requests)
+        # Take largest update timestamp of any reply
+        timestamp = max(int(reply[0][0]) for reply in replies)
+        _, chunk = await receiver.next_complete_chunk(timestamp)
+        assert isinstance(chunk.data, np.ndarray)  # Keep mypy happy
+        for task, bl_idx in zip(tasks, autos):
+            out[task[0]] = chunk.data[:, bl_idx, 0]  # Only keep real part
+        receiver.stream.add_free_chunk(chunk)
+
+    with np.nditer([freqs, amplitude], flags=["multi_index"]) as it:
+        for f, a in it:
+            tasks.append((it.multi_index, f"cw({a}, {f});"))
+            if len(tasks) == correlator.n_inputs:
+                await flush()
+                tasks = []
+    if tasks:
+        await flush()
+    return out
