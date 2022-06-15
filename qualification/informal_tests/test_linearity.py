564b5455584bdca6f6e978aea8b27c03942fb431
avanderbyl <avanderbyl@sarao.ac.za>
2022-06-15 08:26:32 +0000

    Changes as per PR 292.

--- qualification/informal_tests/test_linearity.py
+++ qualification/informal_tests/test_linearity.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 ################################################################################
 
-"""CBF lineraity test."""
+"""CBF linearity test."""
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
@@ -23,7 +23,7 @@ from .. import BaselineCorrelationProductsReceiver, CorrelatorRemoteControl, ant
 from ..reporter import Reporter
 
 
-async def test_cbf_linearity(
+async def test_linearity(
     correlator: CorrelatorRemoteControl,
     receive_baseline_correlation_products: BaselineCorrelationProductsReceiver,
     pdf_report: Reporter,
@@ -41,37 +41,27 @@ async def test_cbf_linearity(
     pdf_report.step("Capture channelised data for various input CW scales and check linearity.")
 
     pdf_report.step("CW scales selection.")
-    cw_scales = [1, 0.5, 0.25, 0.125, 0.0625, 0.03125, 0.015625, 0.0078125, 0.00390625, 0.001953125]
+    cw_scales = [0.5**i for i in range(10)]
     pdf_report.detail(f"CW scales: {cw_scales}")
 
     pdf_report.step("Channel selection. Compute desired channel frequency for Dsim.")
-    rng = np.random.default_rng(seed=2021)
-    random_channel_center = int(rng.uniform(0, correlator.n_chans))
-    pdf_report.detail(f"Random channel selected: {random_channel_center}")
-    pdf_report.detail(
-        f"Channel frequency for selected channel: {round((random_channel_center*(856e6/correlator.n_chans))/1e6,2)}MHz"
-    )
+    sel_chan_center = correlator.n_chans // 3
+    pdf_report.detail(f"Random channel selected: {sel_chan_center}")
+    pdf_report.detail(f"Channel frequency: {(sel_chan_center*(correlator.bandwidth/correlator.n_chans))/1e6:.2f} MHz")
 
     pdf_report.step("Set EQ gain.")
     gain = 0.003
     pdf_report.detail(f"Setting gain to: {gain}")
     await correlator.product_controller_client.request("gain-all", "antenna_channelised_voltage", gain)
 
-    base_corr_prod = []
-    for scale in cw_scales:
-        base_corr_prod.append(
-            await antenna_channelised_voltage.sample_tone_response(
-                rel_freqs=random_channel_center,
-                amplitude=scale,
-                correlator=correlator,
-                receiver=receive_baseline_correlation_products,
-            )
-        )
+    base_corr_prod = await antenna_channelised_voltage.sample_tone_response(
+        rel_freqs=sel_chan_center,
+        amplitude=cw_scales,
+        correlator=correlator,
+        receiver=receive_baseline_correlation_products,
+    )
 
-    linear_scale_result = []
-    for product in base_corr_prod:
-        linear_scale_result.append(product[random_channel_center])
-
+    linear_scale_result = base_corr_prod[:, sel_chan_center]
     linear_test_result = np.sqrt(linear_scale_result / np.max(linear_scale_result))
 
     pdf_report.step("Compute Mean Square Error (MSE).")
@@ -82,32 +72,20 @@ async def test_cbf_linearity(
     # Generate plot with reference
     # TODO: This plot should be automagically added into the report. Not yet sure how to do that.
     plt.figure()
-    plt.plot(10 * np.log10(np.square(cw_scales)), label="Ref")
-    plt.plot(10 * np.log10(np.square(linear_test_result)), label="CBF Lin Test")
+    plt.plot(10 * np.log10(np.square(cw_scales)), label="Reference")
+    plt.plot(10 * np.log10(np.square(linear_test_result)), label="Measured")
     plt.legend()
     plt.xlabel("CW Scale")
     plt.ylabel("dB")
     plt.title("CBF Linearity Test")
-    plt.text(0, -50, f"Channel Under Test: {random_channel_center}", color="green", style="italic")
+    plt.text(0, -50, f"Channel Under Test: {sel_chan_center}", color="green", style="italic")
     plt.text(
         0,
         -53,
-        f"Channel Frequency: {round((random_channel_center*(856e6/correlator.n_chans))/1e6,2)}MHz",
+        f"Channel Frequency: {(sel_chan_center*(correlator.bandwidth/correlator.n_chans))/1e6:.2f} MHz",
         color="green",
         style="italic",
     )
-    labels = np.round(cw_scales, 5)
-    labels = [
-        "$2^{0}$",
-        "$2^{-1}$",
-        "$2^{-2}$",
-        "$2^{-3}$",
-        "$2^{-4}$",
-        "$2^{-5}$",
-        "$2^{-6}$",
-        "$2^{-7}$",
-        "$2^{-8}$",
-        "$2^{-9}$",
-    ]
-    plt.xticks(np.arange(0, len(linear_test_result), step=1), labels=labels)
+    labels = [f"$2^{{-{i}}}$" for i in range(len(cw_scales))]
+    plt.xticks(np.arange(len(linear_test_result)), labels=labels)
     plt.savefig("linearity.png")
