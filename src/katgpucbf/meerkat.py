61fcb44f23d7a5276b85c9139f23bc9e1379e1c7
Bruce Merry <bmerry@sarao.ac.za>
2023-02-09 13:43:55 +0200

    Fix spelling of "centre" in meerkat.py

--- src/katgpucbf/meerkat.py
+++ src/katgpucbf/meerkat.py
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (c) 2022, National Research Foundation (SARAO)
+# Copyright (c) 2022-2023, National Research Foundation (SARAO)
 #
 # Licensed under the BSD 3-Clause License (the "License"); you may not use
 # this file except in compliance with the License. You may obtain a copy
@@ -31,4 +31,9 @@ class Band:
 BANDS = {
     "l": Band("L", adc_sample_rate=1712e6, centre_frequency=1284e6),
     "u": Band("UHF", adc_sample_rate=1088e6, centre_frequency=816e6),
+    "s0": Band("S0", adc_sample_rate=1750e6, centre_frequency=2187.5e6),
+    "s1": Band("S1", adc_sample_rate=1750e6, centre_frequency=2406.25e6),
+    "s2": Band("S2", adc_sample_rate=1750e6, centre_frequency=2625.0e6),
+    "s3": Band("S3", adc_sample_rate=1750e6, centre_frequency=2843.75e6),
+    "s4": Band("S4", adc_sample_rate=1750e6, centre_frequency=3062.5e6),
 }
