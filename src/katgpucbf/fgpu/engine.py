5578a8912ad587d4bd83994eef4be64ea8eb55dc
Bruce Merry <bmerry@sarao.ac.za>
2023-03-30 10:15:05 +0200

    Refactor a function to reduce line wrapping

--- src/katgpucbf/fgpu/engine.py
+++ src/katgpucbf/fgpu/engine.py
@@ -477,11 +477,9 @@ class Pipeline:
     def _init_delay_gain(self) -> None:
         """Initialise the delays and gains."""
         for pol in range(N_POLS):
-            delay_model = MultiDelayModel(
-                callback_func=partial(
-                    self.update_delay_sensor, delay_sensor=self.engine.sensors[f"{self.output.name}.input{pol}.delay"]
-                )
-            )
+            delay_sensor = self.engine.sensors[f"{self.output.name}.input{pol}.delay"]
+            callback_func = partial(self.update_delay_sensor, delay_sensor=delay_sensor)
+            delay_model = MultiDelayModel(callback_func)
             self.delay_models.append(delay_model)
 
         for pol in range(N_POLS):
