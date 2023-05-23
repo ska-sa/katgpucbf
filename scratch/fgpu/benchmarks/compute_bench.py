cada469a73bdedf5a439857fa63ae1e9f67e892f
Bruce Merry <bmerry@sarao.ac.za>
2023-05-23 14:40:09 +0200

    Make compute_bench work with narrowband

--- scratch/fgpu/benchmarks/compute_bench.py
+++ scratch/fgpu/benchmarks/compute_bench.py
@@ -22,19 +22,23 @@ import numpy as np
 from katsdpsigproc import accel
 
 from katgpucbf import DIG_SAMPLE_BITS, N_POLS
-from katgpucbf.fgpu.compute import ComputeTemplate
-from katgpucbf.fgpu.engine import generate_pfb_weights
+from katgpucbf.fgpu.compute import ComputeTemplate, NarrowbandConfig
+from katgpucbf.fgpu.engine import generate_ddc_weights, generate_pfb_weights
 
 
-def main():
+def main():  # noqa: C901
     parser = argparse.ArgumentParser()
     parser.add_argument("--taps", type=int, default=16)
-    parser.add_argument("--samples", type=int, default=16 * 1024 * 1024)
+    parser.add_argument("--src-chunk-samples", type=int, default=32 * 1024 * 1024)
+    parser.add_argument("--dst-chunk-jones", type=int, default=8 * 1024 * 1024)
     parser.add_argument("--channels", type=int, default=32768)
     parser.add_argument("--spectra-per-heap", type=int, default=256)
     parser.add_argument("--dig-sample-bits", type=int, default=DIG_SAMPLE_BITS)
     parser.add_argument("--passes", type=int, default=1000)
-    parser.add_argument("--kernel", choices=["all", "pfb_fir", "fft", "postproc"], default="all")
+    parser.add_argument("--ddc-taps", type=int, default=128)
+    parser.add_argument("--narrowband", action="store_true")
+    parser.add_argument("--narrowband-decimation", type=int, default=8)
+    parser.add_argument("--kernel", choices=["all", "ddc", "pfb_fir", "fft", "postproc"], default="all")
     parser.add_argument(
         "--sem",
         type=int,
@@ -51,14 +55,27 @@ def main():
     rng = np.random.default_rng(seed=1)
     context = accel.create_some_context(device_filter=lambda device: device.is_cuda)
     with context:
-        template = ComputeTemplate(context, args.taps, args.channels, args.dig_sample_bits, narrowband=None)
+        if args.narrowband:
+            narrowband_config = NarrowbandConfig(
+                decimation=args.narrowband_decimation, taps=args.ddc_taps, mix_frequency=0.25
+            )
+            spectra_samples = 2 * args.channels * args.narrowband_decimation
+            window = args.taps * spectra_samples + args.ddc_taps - args.narrowband_decimation
+        else:
+            narrowband_config = None
+            spectra_samples = 2 * args.channels
+            window = args.taps * spectra_samples
+        template = ComputeTemplate(
+            context, args.taps, args.channels, args.dig_sample_bits, narrowband=narrowband_config
+        )
         command_queue = context.create_tuning_command_queue()
-        extra_samples = (args.taps - 1) * args.channels * 2
-        spectra = args.samples // (args.channels * 2)
+        out_spectra = accel.roundup(args.dst_chunk_jones // args.channels, args.spectra_per_heap)
+        frontend_spectra = min(args.src_chunk_samples // spectra_samples, out_spectra)
+        extra_samples = window - spectra_samples
         fn = template.instantiate(
             command_queue,
-            samples=args.samples + extra_samples,
-            spectra=spectra,
+            samples=args.src_chunk_samples + extra_samples,
+            spectra=out_spectra,
             spectra_per_heap=args.spectra_per_heap,
         )
         fn.ensure_all_bound()
@@ -66,6 +83,11 @@ def main():
         h_weights = fn.buffer("weights").empty_like()
         h_weights[:] = generate_pfb_weights(2 * args.channels, args.taps, 1.0)
         fn.buffer("weights").set(command_queue, h_weights)
+
+        if args.narrowband:
+            h_ddc_weights = fn.buffer("ddc_weights").empty_like()
+            h_ddc_weights[:] = generate_ddc_weights(args.ddc_taps, args.narrowband_decimation, 0.005)
+            fn.buffer("ddc_weights").set(command_queue, h_ddc_weights)
 
         h_gains = fn.buffer("gains").empty_like()
         h_gains[:] = 1
@@ -83,8 +105,11 @@ def main():
         for name in ["fine_delay", "phase"]:
             fn.buffer(name).zero(command_queue)
 
+        def run_ddc():
+            fn.ddc[0]()
+
         def run_pfb_fir():
-            fn.pfb_fir[0].spectra = spectra
+            fn.pfb_fir[0].spectra = frontend_spectra
             fn.pfb_fir[0]()
 
         def run_fft():
@@ -94,13 +119,17 @@ def main():
             fn.postproc()
 
         def run_all():
-            fn.run_wideband_frontend(
-                [fn.buffer("in0"), fn.buffer("in1")],
-                [fn.buffer("dig_total_power0"), fn.buffer("dig_total_power1")],
-                [0, 0],
-                0,
-                spectra,
-            )
+            if args.narrowband:
+                fn.run_ddc([fn.buffer("in0"), fn.buffer("in1")], 0)
+                fn.run_narrowband_frontend([0, 0], 0, frontend_spectra)
+            else:
+                fn.run_wideband_frontend(
+                    [fn.buffer("in0"), fn.buffer("in1")],
+                    [fn.buffer("dig_total_power0"), fn.buffer("dig_total_power1")],
+                    [0, 0],
+                    0,
+                    frontend_spectra,
+                )
             fn.run_backend(fn.buffer("out"), fn.buffer("saturated"))
 
         run = locals()[f"run_{args.kernel}"]
