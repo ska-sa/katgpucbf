ae28694cb4df234699def800f2cd2fdd0636f828
Bruce Merry <bmerry@sarao.ac.za>
2023-10-03 13:34:22 +0000

    benchmark_fgpu: add placeholder logit model for n==2
    The scale parameter is just a thumb-suck for now, chosen to be somewhere
    between the n==1 and n==4 parameters. It still needs some proper
    benchmarking.

--- scratch/benchmarks/benchmark_fgpu.py
+++ scratch/benchmarks/benchmark_fgpu.py
@@ -350,6 +350,7 @@ async def search(args: argparse.Namespace) -> tuple[float, float]:
     # The magic numbers are determined from fit.py.
     slope = {
         1: -342.212919,
+        2: -400.0,  # TODO: need to measure
         4: -582.668296,
     }[args.n]
     mid_rates = 0.5 * (rates[:-1] + rates[1:])  # Rates in the middle of the intervals
