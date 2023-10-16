b2975ae37bd31d44a301690661f1264b791adac8
Bruce Merry <bmerry@sarao.ac.za>
2023-10-16 10:38:32 +0200

    Fix up compute_bench.py
    It had not been updated since converting to a single input buffer across
    both polarisations.

--- scratch/fgpu/benchmarks/compute_bench.py
+++ scratch/fgpu/benchmarks/compute_bench.py
@@ -21,7 +21,7 @@ import argparse
 import numpy as np
 from katsdpsigproc import accel
 
-from katgpucbf import DIG_SAMPLE_BITS, N_POLS
+from katgpucbf import DIG_SAMPLE_BITS
 from katgpucbf.fgpu.compute import ComputeTemplate, NarrowbandConfig
 from katgpucbf.fgpu.engine import generate_ddc_weights, generate_pfb_weights
 from katgpucbf.fgpu.main import DEFAULT_DDC_TAPS_RATIO
@@ -35,6 +35,7 @@ def main():  # noqa: C901
     parser.add_argument("--channels", type=int, default=32768)
     parser.add_argument("--spectra-per-heap", type=int, default=256)
     parser.add_argument("--dig-sample-bits", type=int, default=DIG_SAMPLE_BITS)
+    parser.add_argument("--dst-sample-bits", type=int, default=8, choices=[4, 8])
     parser.add_argument("--passes", type=int, default=1000)
     parser.add_argument("--ddc-taps", type=int)  # Default is computed from decimation
     parser.add_argument("--narrowband", action="store_true")
@@ -71,7 +72,7 @@ def main():  # noqa: C901
             spectra_samples = 2 * args.channels
             window = args.taps * spectra_samples
         template = ComputeTemplate(
-            context, args.taps, args.channels, args.dig_sample_bits, narrowband=narrowband_config
+            context, args.taps, args.channels, args.dig_sample_bits, args.dst_sample_bits, narrowband=narrowband_config
         )
         command_queue = context.create_tuning_command_queue()
         out_spectra = accel.roundup(args.dst_chunk_jones // args.channels, args.spectra_per_heap)
@@ -93,12 +94,11 @@ def main():  # noqa: C901
         h_gains[:] = 1
         fn.buffer("gains").set(command_queue, h_gains)
 
-        for i in range(N_POLS):
-            buf = fn.buffer(f"in{i}")
-            h_data = buf.empty_like()
-            assert h_data.dtype == np.uint8
-            h_data[:] = rng.integers(0, 256, size=h_data.shape, dtype=np.uint8)
-            buf.set(command_queue, h_data)
+        buf = fn.buffer("in")
+        h_data = buf.empty_like()
+        assert h_data.dtype == np.uint8
+        h_data[:] = rng.integers(0, 256, size=h_data.shape, dtype=np.uint8)
+        buf.set(command_queue, h_data)
 
         # Fill these with zeros just to ensure performance isn't affected
         # by stray NaNs and the like.
@@ -106,26 +106,26 @@ def main():  # noqa: C901
             fn.buffer(name).zero(command_queue)
 
         def run_ddc():
-            fn.ddc[0]()
+            fn.ddc()
 
         def run_pfb_fir():
-            fn.pfb_fir[0].spectra = frontend_spectra
-            fn.pfb_fir[0]()
+            fn.pfb_fir.spectra = frontend_spectra
+            fn.pfb_fir()
 
         def run_fft():
-            fn.fft[0]()
+            fn.fft()
 
         def run_postproc():
             fn.postproc()
 
         def run_all():
             if args.narrowband:
-                fn.run_ddc([fn.buffer("in0"), fn.buffer("in1")], 0)
+                fn.run_ddc(fn.buffer("in"), 0)
                 fn.run_narrowband_frontend([0, 0], 0, frontend_spectra)
             else:
                 fn.run_wideband_frontend(
-                    [fn.buffer("in0"), fn.buffer("in1")],
-                    [fn.buffer("dig_total_power0"), fn.buffer("dig_total_power1")],
+                    fn.buffer("in"),
+                    fn.buffer("dig_total_power"),
                     [0, 0],
                     0,
                     frontend_spectra,
