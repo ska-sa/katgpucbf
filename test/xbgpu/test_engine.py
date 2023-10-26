ca0f19f45fb3521c6ed042207d542bfc1e07eae0
Amish <apatel@sarao.ac.za>
2023-10-26 13:03:06 +0000

    Remove a type: ignore in xbgpu/test_engine

--- test/xbgpu/test_engine.py
+++ test/xbgpu/test_engine.py
@@ -614,9 +614,7 @@ class TestEngine:
                 np.testing.assert_equal(expected_output, device_result)
             incomplete_accums_counters.append(incomplete_accums_counter)
 
-        xpipelines: list[XPipeline] = [
-            pipeline for pipeline in xbengine._pipelines if isinstance(pipeline, XPipeline)  # type: ignore
-        ]
+        xpipelines: list[XPipeline] = [pipeline for pipeline in xbengine._pipelines if isinstance(pipeline, XPipeline)]
         for pipeline, device_result, incomplete_accums_counter in zip(
             xpipelines, device_results, incomplete_accums_counters
         ):
