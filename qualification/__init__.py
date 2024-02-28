239ef9073c70725e3e5a8c925a145d9e5157c5ba
Bruce Merry <bmerry@sarao.ac.za>
2024-02-28 13:00:40 +0000

    qualification: fix parsing of source_indices

--- qualification/__init__.py
+++ qualification/__init__.py
@@ -405,7 +405,8 @@ class TiedArrayChannelisedVoltageReceiver(XBReceiver):
         self.n_bits_per_sample = cbf.sensors[f"{stream_names[0]}.beng-out-bits-per-sample"].value
         self.timestamp_step = self.n_samples_between_spectra * self.n_spectra_per_heap
         self.source_indices = [
-            ast.literal_eval(cbf.sensors[f"{stream_name}.source-indices"].value) for stream_name in stream_names
+            ast.literal_eval(cbf.sensors[f"{stream_name}.source-indices"].value.decode())
+            for stream_name in stream_names
         ]
 
         self.stream = create_tied_array_channelised_voltage_receive_stream(
