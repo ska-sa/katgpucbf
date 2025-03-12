a43159a5029e28d989f0670384f6e309a3c8ad0b
Bruce Merry <bmerry@sarao.ac.za>
2025-03-12 08:18:50 +0200

    Add a comment to explain a zip(..., strict=False)

--- qualification/tied_array_channelised_voltage/test_linearity.py
+++ qualification/tied_array_channelised_voltage/test_linearity.py
@@ -70,6 +70,10 @@ async def _test_linearity(
     assert scales[middle] == pytest.approx(1.0)
     powers = np.zeros_like(scales)
     for batch in batched(enumerate(scales), len(receiver.stream_names)):
+        # The last batch might be short (since the number of scales won't be
+        # multiple of the number of streams. When zipping with streams, we
+        # don't expect the iterables to be the same length, and some streams
+        # will be unused.
         for (_, scale), beam_name in zip(batch, receiver.stream_names, strict=False):
             await set_variable(client, beam_name, scale)
             pdf_report.detail(f"Set {variable} to {scale} on {beam_name}.")
