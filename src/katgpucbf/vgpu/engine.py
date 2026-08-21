20a0a676cfe8381e19244bb998c90609c1ef9a94
Eaton Emmerich <eemmerich@sarao.ac.za>
2026-08-21 12:21:06 +0200

    print ratio

--- src/katgpucbf/vgpu/engine.py
+++ src/katgpucbf/vgpu/engine.py
@@ -291,6 +291,8 @@ class _CaptureSession:
             it, config.pol_matrix, recv_config.pols, send_config.pols
         )
         it = katcbf_vlbi_resample.resample.Resample(send_config.bandwidth, 0.0, config.resample_parameters, it)
+        logger.info(f"Resample ratio: {self._ratio}")  # type: ignore[attr-defined]
+
         it = katcbf_vlbi_resample.rechunk.Rechunk.align_utc_seconds(it)
         it_rms: katcbf_vlbi_resample.stream.Stream[xr.Dataset] = katcbf_vlbi_resample.power.MeasurePower(it)
         it_rms = RecordPower(it_rms, sensors=self._sensors)
