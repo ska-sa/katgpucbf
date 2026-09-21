0e6b755c216ba488048c480afd90517326878e85
Bruce Merry <bmerry@sarao.ac.za>
2026-09-21 11:56:47 +0200

    test_vlbi: typo fix in docstring

--- qualification/tied_array_resampled_voltage/test_vlbi.py
+++ qualification/tied_array_resampled_voltage/test_vlbi.py
@@ -54,7 +54,7 @@ async def max_retry_test(
     bool
         Whether the test passed.
     int
-        Zero-index attempt (whether successful or not).
+        Zero-indexed attempt (whether successful or not).
     """
     loop = asyncio.get_running_loop()
     for attempt_num in range(max_attempts):
